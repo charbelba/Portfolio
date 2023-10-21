611fce323bbdfa1857d2395ff39206e5494fa38b
charbel bou abdo <120515242+Glompis@users.noreply.github.com>
2023-10-21 14:15:46 +0300

    fixing hero section adding socials fixing details

--- tools.js
+++ tools.js
@@ -3,3 +3,20 @@ function scrollToSection(sectionId) {
     section.scrollIntoView({ behavior: 'smooth' });
 }
 
+const textElement = document.querySelector('.hero-name');
+const text = "Charbel Bou Abdo";
+
+function typeText() {
+    textElement.textContent = '';
+    let i = 0;
+    const typeInterval = setInterval(function () {
+        textElement.textContent += text[i];
+        i++;
+        if (i > text.length) {
+            clearInterval(typeInterval);
+        }
+    }, 100);
+}
+
+// Call typeText to fill in the text
+typeText();
